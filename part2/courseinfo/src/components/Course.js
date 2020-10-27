a46eb869c294f672fc52b51cb0fdd9418a07aa5d
madeinchema <chemavg1@gmail.com>
2020-10-27 21:06:36 +0100

    Completed exercise 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,17 +1,15 @@
-const Header = ({ course }) => <h1>{course.name}</h1>
+const Header = ({ course }) => <h2>{course.name}</h2>
 
 const Content = ({ course }) => (
   <>
-    <Part part={course.parts[0]}/>
-    <Part part={course.parts[1]}/>
-    <Part part={course.parts[2]}/>
+    {course.parts.map(part => <Part part={part} />)}
   </>
 )
 
 const Part = ({ part }) => (
-  <div>
-    <h3>{part.name}</h3>
-    <p>{part.exercises} exercises.</p>
+  <div style={{ margin: '1em'}}>
+    <h3 style={{ marginBottom: '.25em' }}>{part.name}</h3>
+    <p style={{ margin: '0'}}>{part.exercises} exercises.</p>
   </div>
 )
 
@@ -26,6 +24,7 @@ const Total = ({ course }) => {
 const Course = ({ course }) => {
   return (
     <div>
+      <h1>Web development curriculum</h1>
       <Header course={course} />
       <Content course={course}/>
       <Total course={course}/>
