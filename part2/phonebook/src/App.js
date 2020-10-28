0925941f06ef2a6165475be6f2c6a7cc312cf867
madeinchema <chemavg1@gmail.com>
2020-10-28 22:46:34 +0100

    Completed 2.6: The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,43 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { 'Arto Hellas': '040-1234567' }
   ])
-  const [ newName, setNewName ] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons => [...persons, { [newName]: newNumber }]);
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const handleChange = (e, type) => {
+    if (type === 'name') {
+      setNewName(e.target.value);
+    } else if (type === 'number') {
+      setNewNumber(e.target.value);
+    }
+  };
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={(e) => handleChange(e, 'name')}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => handleChange(e, 'number')}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <p><span style={{fontWeight: 'bold'}}>{Object.keys(person)}</span> {Object.values(person)}</p>)}
     </div>
   )
 }
