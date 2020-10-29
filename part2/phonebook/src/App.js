19c44aedc8b6c65b4c4011ad30762c27e3df4edc
madeinchema <chemavg1@gmail.com>
2020-10-29 08:00:47 +0100

    Completed 2.9: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { 'Arto Hellas': '040-1234567' }
-  ])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (persons.some(person => newName in person)) {
+    if (persons.some(person => newName === person.name)) {
       alert(`${newName} is already added to phonebook`);
+    } else if (!newNumber || !newName) {
+      alert(`Please fill the following field${!newNumber && !newName ? 's' : ''}:
+        ${!newName ? 'Name' : ''}
+        ${!newNumber && 'Number'}
+      `);
     } else {
-      setPersons(persons => [...persons, { [newName]: newNumber }]);
+      setPersons(persons => [...persons, { name: newName, number: newNumber }]);
     }
     setNewName('');
     setNewNumber('');
@@ -23,25 +32,39 @@ const App = () => {
       setNewName(e.target.value);
     } else if (type === 'number') {
       setNewNumber(e.target.value);
+    } else if (type === 'search') {
+      setSearch(e.target.value);
     }
   };
 
+  const filterSearch = (item) => {
+    return item.filter(person => person.name.toLowerCase().search(search) !== -1);
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Search by name: <input value={search} onChange={(e) => handleChange(e, 'search')}/>
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={(e) => handleChange(e, 'name')}/>
+          Name: <input value={newName} onChange={(e) => handleChange(e, 'name')}/>
         </div>
         <div>
-          number: <input value={newNumber} onChange={(e) => handleChange(e, 'number')}/>
+          Number: <input value={newNumber} onChange={(e) => handleChange(e, 'number')}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={Object.keys(person)}><span style={{fontWeight: 'bold'}}>{Object.keys(person)}</span> {Object.values(person)}</p>)}
+      {filterSearch(persons).map(person => (
+        <p key={person.name}>
+          <span style={{fontWeight: 'bold'}}>{person.name}</span> {person.number}
+        </p>
+      ))}
     </div>
   )
 }
